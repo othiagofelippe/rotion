4c306fb50a8331b6dc5a27a6819c413b21bc9e80
othiagofelippe <thiagofelippe135@gmail.com>
2022-12-21 00:00:59 -0300

    TypeScript & ESLint

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,6 @@
-import './styles/global.css';
+import React from 'react'
+import './styles/global.css'
 
 export function App() {
-  return (
-    <div className='flex'>
-      <h1 className="">Hello World</h1>
-      <h1 className="">Hello World</h1>
-    </div>
-  );
+  return <h1 className="">Hello World</h1>
 }
-
